ec202282c739396278a80c7a2e6bc10e73c221f2
slivka- <slivka104@gmail.com>
2017-12-09 15:53:15 +0100

    Initial file structure

---
+++ 108222/src/java/IPlateRemote.java
@@ -0,0 +1,7 @@
+/**
+ * @author Michał Śliwa
+ */
+public interface IPlateRemote
+{
+    
+}
---
+++ PlateClient/src/java/AppClient.java
@@ -0,0 +1,15 @@
+/**
+ * @author Michał Śliwa
+ */
+public class AppClient
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+    
+}
